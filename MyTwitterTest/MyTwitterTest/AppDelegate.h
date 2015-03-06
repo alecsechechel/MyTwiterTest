34b8d0fd0ed436cc6a758daf6addb5e5b14ca758
Alex <alecsechechel@gmail.com>
2015-03-06 16:28:51 +0200

    test

---
+++ MyTwitterTest/MyTwitterTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyTwitterTest
+//
+//  Created by Alex on 06.03.15.
+//  Copyright (c) 2015 Oleksii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyTwitterTest/MyTwitterTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyTwitterTest
+//
+//  Created by Alex on 06.03.15.
+//  Copyright (c) 2015 Oleksii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
